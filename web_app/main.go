95e006d43c72bec50fc65e0257a0bf6b8cd08ed4
takumi2786 <takumi_1884@icloud.com>
2020-09-15 05:26:56 +0900

    Lesson88

--- web_app/main.go
+++ web_app/main.go
@@ -42,10 +42,13 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):] //スライス的な使い方？
 	// println(title)
-	p, _ := loadPage(title)
-	// wというファイルに情報を書き込んでいる．
-	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body) //htmlのページとして表示される．
-	renderTemplate(w, "view", p)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body) //htmlのページとして表示される．// w変数に情報を書き込んでいる．
+	renderTemplate(w, "view", p) // これは，テンプレートファイルをコンパイルして，wに情報を書き込んでいる．
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,19 +58,23 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil { //editの場合は，ページファイルが存在しなくても，新規で作成
 		p = &Page{Title: title}
 	}
-	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body) //htmlのページとして表示される．
-	// wというファイルに情報を書き込んでいる．
-	renderTemplate(w, "edit", p)
+	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body) //htmlのページとして表示される．// w変数に情報を書き込んでいる．
+	renderTemplate(w, "edit", p) // これは，テンプレートファイルをコンパイルして，wに情報を書き込んでいる．
 }
 
-// httpハンドラの作成
-// type testHandler struct {
-// }
-
-// func (th testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Println("yeah")
-// 	fmt.Fprintf(w, "<div>I am testHandler</div>") //htmlのページとして表示される．
-// }
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):] //スライス的な使い方？
+	body := r.FormValue("body")         //name==bodyのフォーム情報を取得
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// リダイレクト，つまりはページ移動．
+	// ３つめの引数は何？
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
 
 // ウェブサーバの立ち上げ
 func main() {
@@ -75,15 +82,12 @@ func main() {
 	// http://localhost:8080/view/へのアクセスがあった場合に，viewHandlerを実行する．
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+
 	// http.ListenAndServe: :8080が生きている限り，サーバを立ち上げ続ける？
 	// 第一引数：ポート
 	// 第二引数：ハンドラ
 	// log.Fatal：errorがあった場合に，ログとして表示する関数．
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
-	// httpハンドラの作成
-	// var testHandler http.Handler
-	// log.Fatal(http.ListenAndServe(":8080", testHandler))
-	// 実行はできるが，ハンドラは動かない．これはいつ実行される？
 
 }
